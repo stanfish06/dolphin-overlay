476335d653b8e576ed5687c056e0086bcb7a2ef7
Stan <zyyu@umich.edu>
2026-06-13 16:23:16 -0400

    fix kservice nixpkgs 26.5

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@ final: prev: {
         rm $out/bin/dolphin
         makeWrapper ${kprev.dolphin}/bin/dolphin $out/bin/dolphin \
           --set XDG_CONFIG_DIRS "${prev.libsForQt5.__internalKF5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
-          --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
+          --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.__internalKF5.kservice}/etc/xdg/menus/applications.menu"
       '';
     };
   });
